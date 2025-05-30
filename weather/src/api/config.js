d524614e400559aff4f05c955bfcd42cbf9bb0c0
rawaha <rawahaahmed081@gmail.com>
2025-05-31 01:11:33 +0500

    commit

--- weather/src/api/config.js
+++ weather/src/api/config.js
@@ -1,9 +1,10 @@
 export const ApiConfig = {
     BaseURL: "https://api.openweathermap.org/data/2.5",    
-    GeoCoding: 'http://api.openweathermap.org/geo/1.0',        
+GeoCoding: 'https://api.openweathermap.org/geo/1.0',
     APIKey: import.meta.env.VITE_API_KEY,                      
     DefaultParams: {
         units: 'metric',
-        appid: import.meta.env.VITE_API_KEY                    
+        appid: import.meta.env.VITE_API_KEY,                    
     }
 };
+
