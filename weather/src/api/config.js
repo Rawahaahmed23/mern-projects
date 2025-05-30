91551187eb06011bb67286b9326e37e6303ec000
Rawaha Ahmed <143651775+Rawahaahmed23@users.noreply.github.com>
2025-05-31 01:15:06 +0500

    Update config.js

--- weather/src/api/config.js
+++ weather/src/api/config.js
@@ -1,6 +1,6 @@
 export const ApiConfig = {
     BaseURL: "https://api.openweathermap.org/data/2.5",    
-    GeoCoding: 'http://api.openweathermap.org/geo/1.0',        
+    GeoCoding: 'https://api.openweathermap.org/geo/1.0',        
     APIKey: import.meta.env.VITE_API_KEY,                      
     DefaultParams: {
         units: 'metric',
