256228dc17c24a90da765693b4d50a3fa682902a
Rawahaahmed23 <rawahaahmed081@gmail.com>
2025-07-30 21:37:22 -0700

    'commit'

--- attendenceapp/client/src/pages/Home.jsx
+++ attendenceapp/client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Button } from "../components/ui/button"
 import { Card, CardContent } from "../components/ui/Card"
 
 
-import { Badge } from "../components/ui/badge"
+
 
 import { Badge } from "lucide-react"
 
