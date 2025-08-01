2fad90fa00cbe2308be2dbb89ff56be2edb044c0
rawaha <rawahaahmed081@gmail.com>
2025-08-01 23:05:59 +0500

    'commit'

--- attendenceapp/client/src/store/useAuth.jsx
+++ attendenceapp/client/src/store/useAuth.jsx
@@ -30,6 +30,7 @@ export const UserProvider = ({ children }) => {
       try {
         const response = await fetch("https://mern-projects-production-9252.up.railway.app/user", {
             method: "GET",
+                 credentials: "include",
           headers: {
     Authorization: `Bearer ${token}`,
   },
