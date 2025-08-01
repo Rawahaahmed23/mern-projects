c6fe7740eb1960b176c269edba60f1a6949c43db
rawaha <rawahaahmed081@gmail.com>
2025-08-02 01:40:51 +0500

    'cmmit'

--- attendenceapp/backend/server.js
+++ attendenceapp/backend/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
 'http://localhost:5173',
-    'https://mern-projects-oupo.vercel.app/',
+    'https://mern-projects-oupo.vercel.app',
   ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
   credentials: true
