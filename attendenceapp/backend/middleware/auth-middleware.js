ea593d4b2fe880d96cdbf732978629a19aa18afe
Rawahaahmed23 <rawahaahmed081@gmail.com>
2025-08-04 20:59:10 -0700

    'commit'

--- attendenceapp/backend/middleware/auth-middleware.js
+++ attendenceapp/backend/middleware/auth-middleware.js
@@ -8,6 +8,11 @@ const authMiddleware = async (req, res, next) => {
   try {
     
     let token =req.cookies.token;
+
+
+        if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    }
     if (!token) {
       return res.status(400).json({ msg: 'token missing' });
     }
